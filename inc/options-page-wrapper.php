a5edaaccb30b4f1d0edb18ff4cf13b83629c2586
Cyrus E <devcyruse@gmail.com>
2019-06-27 13:47:54 -0400

    plugin along with widget and shortcode fully functional

--- inc/options-page-wrapper.php
+++ inc/options-page-wrapper.php
@@ -56,18 +56,32 @@
 								<div class="inside">
 									<p>Below are your 20 most recent badges</p>
 									<ul class="wptreehouse-badges">
-										<?php for( $i = 0; $i < 20; $i++ ): ?>
+									<?php 
+										$total_badges = count( $wptreehouse_profile->{'badges'} );
+										for( $i = $total_badges - 1; $i >= $total_badges - 20; $i-- ):		
+										;?>
 											<li>
 												<ul>
 													<li>	
-														<img width="120px" src="<?php echo $plugin_url . '/images/wp-badge.png'; ?>">								
-													</li>										
+														<img width="120px" src="<?php echo $wptreehouse_profile->{'badges'}[$i]->{'icon_url'}; ?>">								
+													</li>
+													<?php if ( $wptreehouse_profile->{'badges'}[$i]->{'url'} != $wptreehouse_profile->{'profile_url'} ) : ?>									
+														<li class="wptreehouse-badge-name">
+
+															<a href="<?php echo $wptreehouse_profile->{'badges'}[$i]->{'courses'}[1]->{'url'}; ?>">
+																<?php echo $wptreehouse_profile->{'badges'}[$i]->{'name'}; ?>
+															</a>
+														</li>
+														<li class="wptreehouse-project-name">
+															<a href="<?php echo $wptreehouse_profile->{'badges'}[$i]->{'courses'}[0]->{'url'}; ?>">
+																<?php echo $wptreehouse_profile->{'badges'}[$i]->{'courses'}[0]->{'title'}; ?>
+															</a>
+														</li>
+													<?php else: ?>
 													<li class="wptreehouse-badge-name">
-														<a href="#">Badge Name</a>
+															<?php echo $wptreehouse_profile->{'badges'}[$i]->{'name'}; ?>
 													</li>
-													<li class="wptreehouse-project-name">
-														<a href="#">Project Name</a>
-													</li>
+													<?php endif; ?>														
 												</ul>									
 											</li>								
 										<?php endfor; ?>
@@ -76,14 +90,28 @@
 								<!-- .inside -->
 						<?php endif; ?>
 					</div><!-- .postbox -->
-					<div class="postbox">
-						<h2 class="hndle"><span>Most Recent Badges</span></h2>
-						<div class="inside">
-							<pre><code>
-								<?php var_dump( $wptreehouse_profile ); ?>
-							</code></pre>
+					<?php if ( $display_json == true ) : ?>
+						<div class="postbox">
+							<h2 class="hndle"><span>JSON Feed</span></h2>
+							<div class="inside">
+								<p>
+									<?php echo $wptreehouse_profile->{'name'}; ?>
+								</p>
+								<p>
+									<?php echo $wptreehouse_profile->{'profile_url'}; ?>
+								</p>
+								<p>
+									<?php echo $wptreehouse_profile->{'badges'}[0]->{'name'}; ?>
+								</p>		
+								<p>
+									<?php echo $wptreehouse_profile->{'badges'}[1]->{'courses'}[1]->{'title'}; ?>
+								</p>				
+								<pre><code>
+									<?php var_dump( $wptreehouse_profile ); ?>
+								</code></pre>
+							</div>
 						</div>
-					</div>
+					<?php endif; ?>
 				</div>
 				<!-- .meta-box-sortables .ui-sortable -->
 
@@ -96,12 +124,12 @@
 				<div class="meta-box-sortables">
 					<?php if(isset($wptreehouse_username) && $wptreehouse_username != '') : ?>
 					<div class="postbox">
-							<h3><span>Mike the Frog's Profile</span></h3>
+							<h3><span><?php echo $wptreehouse_profile->{'name'}; ?>'s Profile</span></h3>
 							<div class="inside">						
-							<p><img width="100%" class="wptreehouse-gravatar" src="<?php echo $plugin_url . '/images/mike-the-frog.png'; ?>" alt="Mike the Frog Gravatar"></p>
+							<p><img width="100%" class="wptreehouse-gravatar" src="<?php echo $wptreehouse_profile->{'gravatar_url'}; ?>" alt="Mike the Frog Gravatar"></p>
 								<ul class="wptreehouse-badges-and-points">							
-									<li>Badges: <strong>200</strong></li>
-									<li>Points: <strong>10000</strong></li>
+									<li>Badges: <strong><?php echo count($wptreehouse_profile->{'badges'}); ?></strong></li>
+									<li>Points: <strong><?php echo $wptreehouse_profile->{'points'}->{'total'}; ?></strong></li>
 								</ul>
 								<form name= "wptreehouse_username_form" method="post" action="">
 									<input type= "hidden" name="wptreehouse_form_submitted" value="Y">
--- wptreehouse-badges.php
+++ wptreehouse-badges.php
@@ -18,6 +18,7 @@
 */
 $plugin_url = WP_PLUIGIN_URL . '/wptreehouse-badges' ;
 $options = array();
+$display_json = true;
 
 /*
  *	Add a link to our plugin in the admin menu
@@ -51,6 +52,7 @@ function wptreehouse_badges_options_page() {
 	}
     global $plugin_url; //we make this var global to make it available to reference in other files in same directory
     global $options;
+    global $display_json;
     if(isset($_POST['wptreehouse_form_submitted'])) {
         $hidden_field = esc_html ( $_POST['wptreehouse_form_submitted'] );
         if ( $hidden_field == 'Y' ) {
@@ -69,22 +71,134 @@ function wptreehouse_badges_options_page() {
         $wptreehouse_username = $options['wptreehouse_username']; //this would keep the user input after logging in
         $wptreehouse_profile = $options['wptreehouse_profile'];
     }
-    var_dump($wptreehouse_profile);
+    //var_dump($wptreehouse_username);
     require('inc/options-page-wrapper.php');
     
 }
 
+class Wptreehouse_Badges_Widgets extends WP_Widget { //copied skeleton from wp widget codex
+
+	function wptreehouse_badges_widgets() {
+		// Instantiate the parent object
+		parent::__construct( false, 'Official Treehouse Badges Widget' );
+	}
+
+	function widget( $args, $instance ) {
+        // Widget output
+        
+        extract( $args ); //we need to include this in our code in order for it to work
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        $num_badges = $instance['num_badges'];
+        $display_tooltip = $instance['display_tooltip'];
+        $options = get_option( 'wptreehouse_badges' );
+        $wptreehouse_profile = $options['wptreehouse_profile'];
+
+        require ( 'inc/front-end.php' );
+	}
+
+	function update( $new_instance, $old_instance ) {
+        // Save widget options
+        
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']); //strip tags will not display any html if it was inputted in the input box
+        $instance['num_badges'] = strip_tags($new_instance['num_badges']); 
+        $instance['display_tooltip'] = strip_tags($new_instance['display_tooltip']); 
+        return $instance;
+	}
+
+	function form( $instance ) {
+        // Output admin widget options form
+        $title = esc_attr( $instance['title']);
+        $num_badges = esc_attr( $instance['num_badges']);
+        $display_tooltip = esc_attr( $instance['display_tooltip']);
+
+        $options = get_option( 'wptreehouse_badges' );
+        $wptreehouse_profile = $options['wptreehouse_profile'];
+        require ( 'inc/widget-fields.php' );
+	}
+}
+
+function wptreehouse_badges_register_widgets() {
+	register_widget( 'Wptreehouse_Badges_Widgets' );
+} 
+
+add_action( 'widgets_init', 'wptreehouse_badges_register_widgets' ); //end widget
+
+//begin  shortcode
+function wptreehouse_badges_shortcode($atts, $content = null) {
+    global $post; //define post object to get info about post in which the shortcode appears
+
+    extract( shortcode_atts( array(
+        'num_badges' => '8',
+        'tooltip' => 'on'
+    ), $atts) );
+    if ( $tooltip == 'on' ) $tooltip = 1;
+    if ( $tooltip == 'off' ) $tooltip = 0;
+
+    $display_tooltip = $tooltip;
+    $options = get_option( 'wptreehouse_badges' );
+    $wptreehouse_profile = $options['wptreehouse_profile'];
+
+    //below process is called buffering and needed for shortcodes; it holds the require function below until all above info has been run and then runs the require function below
+    ob_start();
+    require ( 'inc/front-end.php' );
+    $content = ob_get_clean();
+    return $content;
+}
+
+add_shortcode('wptreehouse_badges', 'wptreehouse_badges_shortcode'); //shortcode end, we used a plugin to show/hide widgets on specific pages
+
+//function to fetch json data of inputted username 
 function wptreehouse_badges_get_profile($wptreehouse_username) {
     $json_feed_url = 'http://teamtreehouse.com/' . $wptreehouse_username . '.json';
     $args = array( 'timeout' => 120 ); //we settimeout to give enough time for all data to load
     $json_feed = wp_remote_get($json_feed_url, $args);
-    $wptreeouse_profile = json_decode($json_feed['body']); //decode will translate meaningless numbers into understandable values, without adding body, we will get way more than json
+    $wptreehouse_profile = json_decode($json_feed['body']); //decode will translate meaningless numbers into understandable values, without adding body, we will get way more than json
     return $wptreehouse_profile; 
 }
 
-function wptreehouse_badges_styles() {
-    wp_enqueue_style( 'wptreehouse_badges_style', plugins_url( 'wptreehouse-badges/wptreehouse-badges.css' ) );
+
+//function to make the plugin update automatic instead of user pressing update button using server side, which is lenghty/ refresh the profile by Adding AJAX To Plugins on the Front-End
+function wptreehouse_badges_refresh_profile() {
+    $options = get_option( 'wptreehouse_badges' );
+    $last_updated = $options['last_updated'];
+    $current_time = time();
+    $update_difference = $current_time - $last_updated;
+
+    if ( $update_difference > 86400 ) { //86400 is number of seconds in a day
+        $wptreehouse_username = $options['wptreehouse_username'];
+        $options['wptreehouse_profile'] = wptreehouse_badges_get_profile($wptreehouse_username);
+        $options['last_updated'] = time();
+
+        update_option( 'wptreehouse_badges', $options );
+    }
+    die(); //this will let ajax call know that function has ended
 }
-add_action( 'admin_head' , 'wptreehouse_badges_styles')
+add_action('wp_ajax_wptreehouse_badges_refresh_profile', 'wptreehouse_badges_refresh_profile'); //create a custom hook for the ajax function in order to make it accessable for the front end to make ajax calls directly to the plugin
+
+
+function wptreehouse_badges_enable_frontend_ajax() { ?>
+    <script>
+       var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
+    </script>
+<?php } 
+add_action( 'wp_head', 'wptreehouse_badges_enable_frontend_ajax' );
+
+?>
+<?php
+function wptreehouse_badges_backend_styles() {
+    wp_enqueue_style( 'wptreehouse_badges_backend_css', plugins_url( 'wptreehouse-badges/wptreehouse-badges.css' ) );
+}
+add_action( 'admin_head' , 'wptreehouse_badges_backend_styles');
+
+
+function wptreehouse_badges_frontend_scripts_and_styles() {
+    wp_enqueue_style( 'wptreehouse_badges_backend_css', plugins_url( 'wptreehouse-badges/wptreehouse-badges.css' ) );
+    wp_enqueue_script( 'wptreehouse_badges_frontend_css', plugins_url( 'wptreehouse-badges/wptreehouse-badges.js' ), array('jquery'), '', true );
+
+}
+add_action( 'wp_enqueue_scripts' , 'wptreehouse_badges_frontend_scripts_and_styles');
+
+
 
 ?>
